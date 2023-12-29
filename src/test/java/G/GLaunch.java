5f18dd5a40838ad462667c5cf1c2a9276b15c6b9
hchalla2021 <53994609+hchalla2021@users.noreply.github.com>
2023-12-29 19:52:31 +0530

    driver

--- src/test/java/G/GLaunch.java
+++ src/test/java/G/GLaunch.java
@@ -2,10 +2,10 @@ package G;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-public class GLaunch11 {
+public class GLaunch {
 @Test
 public void Launchg() {
-		System.out.println("This is dev ops selenium Project");
+		System.out.println("This is dev ops selenium Project 2023");
 		String path = System.getProperty("user.dir");
 		System.out.println(path);
 		System.setProperty("webdriver.chrome.driver",path+"\\chromedriver.exe");

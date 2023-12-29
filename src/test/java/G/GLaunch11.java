67c714a36c3dcb425af8957dede0cdf5aaba779e
hchalla2021 <53994609+hchalla2021@users.noreply.github.com>
2023-12-29 19:28:18 +0530

    Initial commit

---
+++ src/test/java/G/GLaunch11.java
@@ -0,0 +1,20 @@
+package G;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class GLaunch11 {
+@Test
+public void Launchg() {
+		System.out.println("This is dev ops selenium Project");
+		String path = System.getProperty("user.dir");
+		System.out.println(path);
+		System.setProperty("webdriver.chrome.driver",path+"\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		String gtitle = driver.getTitle();
+		System.out.println("The google title is: " + gtitle);
+		driver.close();
+	}
+
+}
